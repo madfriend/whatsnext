b46ca043470a80cee56f3f8a8cd100f411feb895
Alex Chuchunkov <scarywound@gmail.com>
2017-01-25 22:54:59 +0300

    as pip package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+  name='whatsnext',
+  version='0.1',
+  packages=['whatsnext']
+)
--- whatsnext/whatsnext.py
+++ whatsnext/whatsnext.py
@@ -1,5 +1,5 @@
 #! coding: utf-8
-from parsers import ALL_PARSERS
+from whatsnext.parsers import ALL_PARSERS
 
 def short_fmt(concert):
     return "{}\t{}\t{}".format(
